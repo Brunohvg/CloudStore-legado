be54d0b2b715198a73af3df495c501609d3720cb
Bruno Vidal <brunovidal27.19@gmail.com>
2023-10-30 01:13:48 -0300

    sistema de login

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # from django.views.decorators.http import require_POST
 from django.contrib import messages
@@ -8,6 +9,31 @@ from django.contrib import messages
 
 
 def user_login(request):
+    if request.method == "POST":
+        signin_email = request.POST["signin-email"]
+        signin_password = request.POST["signin-password"]
+
+        try:
+            user_name = User.objects.get(email=signin_email)
+            user_authenticate = authenticate(
+                request, username=user_name, password=signin_password
+            )
+            if user_authenticate is not None:
+                login(request, user=user_authenticate)
+                print("ok")
+                return redirect("user_auth:user_logout")
+            else:
+                messages.add_message(
+                    request, messages.INFO, message="E-mail ou senha incorretos"
+                )
+
+        except User.DoesNotExist:
+            messages.add_message(
+                request,
+                messages.INFO,
+                message="Este e-mail, não existe",
+            )
+
     return render(request, "user_auth/user_login.html")
 
 
@@ -18,9 +44,11 @@ def user_signup(request):
         signup_password = request.POST["signup-password"]
         try:
             user_exists = User.objects.get(username=signup_name)
-            print()
+
             messages.add_message(
-                request, messages.INFO, f"Este usuário já exite: {user_exists}"
+                request,
+                messages.INFO,
+                f"O usuário, {user_exists} já exite no nosso sistema ",
             )
 
         except User.DoesNotExist:
@@ -33,9 +61,9 @@ def user_signup(request):
     return render(request, "user_auth/user_signup.html")
 
 
-def user_logout(request):
-    return render(request, "user_auth/user_logout.html")
-
-
 def password_reset(request):
     return render(request, "user_auth/password_reset.html")
+
+
+def user_logout(request):
+    return render(request, "user_auth/user_logout.html")
